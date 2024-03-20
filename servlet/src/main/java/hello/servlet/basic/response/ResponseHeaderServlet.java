90a7bc1048112b659ba1e1a31ee4a874e5287769
baek.gang.hyeon <weiss1129@gmail.com>
2024-03-20 23:10:27 +0900

    HttpServletResponse - 기본 사용법

---
+++ servlet/src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,57 @@
+package hello.servlet.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // [status-line]
+//        response.setStatus(HttpServletResponse.SC_OK);
+//        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+
+        // [response-header]
+//        response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("my-header", "hello");
+        
+        // [Header 편의 메서드]
+        content(response);
+        cookie(response);
+        redirect(response);
+
+        // [message body]
+        response.getWriter().write("안녕하세요");
+    }
+
+    private void content(HttpServletResponse response) {
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+    }
+
+    private void cookie(HttpServletResponse response) {
+        // Set-Cookie : myCookie=good; Max-Age=600;
+        // response.setHeader("Set-Cookie", "myCookie=good;", Max-Age=600");
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600);
+        response.addCookie(cookie);
+    }
+
+    private void redirect(HttpServletResponse response) throws IOException {
+        // Status Code 302
+        // Location : /basic/hello-form.html
+        response.setStatus(HttpServletResponse.SC_FOUND);
+        // response.setHeader("Location", "/basic/hello-form.html");
+        response.sendRedirect("/basic/hello-form.html");
+    }
+
+}
