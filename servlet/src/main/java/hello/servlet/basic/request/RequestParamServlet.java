f8644ab7d0ba320c3abb581aada90ddff086cf71
baek.gang.hyeon <weiss1129@gmail.com>
2024-03-20 22:18:17 +0900

    HTTP 요청 데이터 - GET 쿼리 파라미터

---
+++ servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,38 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+/**
+ * 1. 파라미터 전송 기능
+ * http://localhost:8080/request-param?username=hello&age=20
+ */
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getParameterNames().asIterator()
+                .forEachRemaining(parameterName -> System.out.println(parameterName + " = " + request.getParameter(parameterName)));
+
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+        System.out.println(username);
+        System.out.println(age);
+
+        String[] usernames = request.getParameterValues("username");
+        for (String name : usernames) {
+            System.out.println("name = " + name);
+        }
+
+        response.getWriter().write("OK");
+
+    }
+}
