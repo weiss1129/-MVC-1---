c14aaa0340cc9dfcdc99c93d2af98853c507c10f
baek.gang.hyeon <weiss1129@gmail.com>
2024-03-24 20:21:16 +0900

    서블릿으로 회원 관리 웹 애플리케이션 만들기

--- servlet/src/main/java/hello/servlet/domain/member/MemberRepository.java
+++ servlet/src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public class MemberRepository {
 
-    private static Map<Long, Member> store = new HashMap<>();
+    private static final Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
 
     private static final MemberRepository instance = new MemberRepository();
