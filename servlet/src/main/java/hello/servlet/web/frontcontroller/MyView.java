045c07d2fabb692a7d43b0c793eb89bcc999c9d3
baek.gang.hyeon <weiss1129@gmail.com>
2024-05-18 17:15:44 +0900

    V3

--- servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
     private String viewPath;
@@ -15,6 +16,12 @@ public class MyView {
     }
 
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        model.forEach(request::setAttribute);
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
     }
